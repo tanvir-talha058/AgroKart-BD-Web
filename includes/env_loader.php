0605619b0997b231623ee117b9f8f304ebdbf4c5
Fahim Faisal Talha <fahimtalha9@gmail.com>
2025-08-16 14:31:44 +0600

    Done

--- includes/env_loader.php
+++ includes/env_loader.php
@@ -1,9 +1,10 @@
 <?php
 // File: includes/env_loader.php
 
-function loadEnv() {
+function loadEnv()
+{
     $env_file = __DIR__ . '/../.env';
-    
+
     if (file_exists($env_file)) {
         $lines = file($env_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         foreach ($lines as $line) {
@@ -11,11 +12,11 @@ function loadEnv() {
             if (strpos(trim($line), '#') === 0) {
                 continue;
             }
-            
+
             list($name, $value) = explode('=', $line, 2);
             $name = trim($name);
             $value = trim($value);
-            
+
             // Set as environment variable
             putenv("$name=$value");
             $_ENV[$name] = $value;
@@ -28,7 +29,8 @@ function loadEnv() {
 loadEnv();
 
 // Helper function to get environment variables
-function env($key, $default = null) {
+function env($key, $default = null)
+{
     $value = getenv($key);
     if ($value === false) {
         return $default;
